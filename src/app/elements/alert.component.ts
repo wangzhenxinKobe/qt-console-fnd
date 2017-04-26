3461f95f090e47009b115146e941e049d73e11ed
Gavin Yu <yuweihua@reganfund.com>
2017-04-26 08:46:14 +0800

    完善"提示框"组件

--- src/app/elements/alert.component.ts
+++ src/app/elements/alert.component.ts
@@ -4,15 +4,20 @@
 
 import {Component} from '@angular/core';
 
+declare var $ : any;
+
 @Component({
-    selector: 'app-alert',
-    template: `
+  selector: 'app-alert',
+  template: `
     
-    <div class="r_tips r_danger" id="danger"><i class="icon fa fa-close"></i> 提交失败！</div>
+    <div class="{{ styles[typeIndex].type }}" id="alert-win">
+       <i class="{{ styles[typeIndex].icon }}"></i> {{message}}
+       <button type="button" (click)="close()" class="close"><span >&times;</span></button>
+    </div>
     
     `,
   styles:[`
-  
+    
   .r_tips{
     position: absolute;
     left: 0;
@@ -44,5 +49,59 @@ import {Component} from '@angular/core';
 })
 
 export class AlertComponent {
+
+  styles = [
+    {
+      icon : 'icon fa fa-check',
+      type : 'r_tips r_success'
+    },
+    {
+      icon : 'icon fa fa-warning',
+      type : 'r_tips r_warning'
+    },
+    {
+      icon : 'icon fa fa-close',
+      type : 'r_tips r_danger'
+    }
+  ];
+
+  typeIndex : number = 0;
+  message : string = 'hello word';
+
+  error(msg : string) {
+
+    this.message = msg;
+    this.typeIndex = 2;
+    this.showMe();
+
+  }
+
+  warn(msg : string) {
+
+    this.message = msg;
+    this.typeIndex = 1;
+    this.showMe();
+
+  }
+
+  info(msg : string) {
+
+    this.message = msg;
+    this.typeIndex = 0;
+    this.showMe();
+
+  }
+
+  close() {
+
+    $('#alert-win').slideUp(400);
+
+  }
+
+  private showMe() {
+
+    $('#alert-win').slideDown(400);
+
+  }
 
 }
