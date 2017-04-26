40c1a4848b1896813eeaf9115d9ded4101f785d7
Gavin Yu <yuweihua@reganfund.com>
2017-04-26 10:35:40 +0800

    针对alert更新样式

--- src/app/elements/alert.component.ts
+++ src/app/elements/alert.component.ts
@@ -15,37 +15,7 @@ declare var $ : any;
        <button type="button" (click)="close()" class="close"><span >&times;</span></button>
     </div>
     
-    `,
-  styles:[`
-    
-  .r_tips{
-    position: absolute;
-    left: 0;
-    right: 0;
-    top: 0;
-    text-align: center;
-    color: #fff;
-    line-height: 150%;
-    padding:  10px;
-    z-index: 99;
-    display: none;	 
-  }
-  .r_tips i{
-    font-size: 18px;
-    vertical-align: middle;
-    margin: -3px 0 0 0;
-  }
-  .r_danger{
-	  background: rgba(221,75,57,0.9);
-  }
-  .r_warning{
-	  background: rgba(243,156,18,0.9);
-  }
-  .r_success{
-	  background: rgba(0,192,239,0.9);
-  }
-  
-  `]
+    `
 })
 
 export class AlertComponent {
